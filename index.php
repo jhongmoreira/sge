fe510790be246a2df9e7ccc41ef086193d7880d6
nathangmoreira <jhongmoreira@gmail.com>
2019-05-24 15:43:38 -0300

    Arquivos base do projeto

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Sistema de Gerenciamento Escolar</title>
+    <meta name="author" content="Jhonathan Gleidsom Moreira / arte.jhonathan@gmail.com"/>
+    <!-- Links CSS próprio -->
+    <link href="css/principal.css" rel="stylesheet" type="text/css"/>
+    <!-- Links CSS Boostrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <!-- CSS Fontawesome -->
+    <link href="bootstrap/fontawesome/css/fontawesome.css" rel="stylesheet" type="text/css"/>
+    <link href="bootstrap/fontawesome/js/regular.css" rel="stylesheet" type="text/css"/>
+    <script src="bootstrap/fontawesome/js/solid.js"></script>
+    <!-- Arquivos JS -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="bootstrap/fontawesome/js/fontawesome.js"></script>
+    <script src="bootstrap/fontawesome/js/regular.js"></script>
+    <script src="bootstrap/fontawesome/js/solid.js"></script>
+
+  </head>
+  <!-- Corpo do site -->
+  <body>
+
+  </body>
+  <!-- Arquivos jQuery -->
+
+  </html>
