e77ee58f0eb37706a7f5fb4c7c6408329b978378
nathangmoreira <jhongmoreira@gmail.com>
2019-05-26 17:41:02 -0300

    Login

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
 <html lang="pt-br">
 <head>
   <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Sistema de Gerenciamento Escolar</title>
   <meta name="author" content="Jhonathan Gleidsom Moreira / arte.jhonathan@gmail.com"/>
   <!-- Links CSS próprio -->
