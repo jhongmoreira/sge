167f6adee79ffc8b04f116216f4ef7f5b597075d
Jhonathan Moreira <jhongmoreira@gmail.com>
2020-07-16 21:13:51 -0300

    Alteracao nome variavel matricula

--- paginas/novo_mv_aluno.php
+++ paginas/novo_mv_aluno.php
@@ -28,7 +28,7 @@
     <div class="col-md-2">
       <div class="form-group">
         <label for="idMatricula">Matricula:</label>
-          <select class="form-control" required="" name="candidato">
+          <select class="form-control" required="" name="matricula">
             <option></option>
             <?php
               $banco->query("SELECT id, matricula FROM alunos");
