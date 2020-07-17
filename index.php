dc6fb388fe75499001176744d51b63b261320d6b
Jhonathan Moreira <jhongmoreira@gmail.com>
2020-07-16 21:50:54 -0300

    Select2 removido

--- index.php
+++ index.php
@@ -25,13 +25,6 @@
     <script src="bootstrap/fontawesome/js/solid.js"></script>
     <!--Select2-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-    <script>
-      $(document).ready(function() {
-          $('.select-alunos').select2();
-      });
-    </script>
   </head>
   <!-- Corpo do site -->
   <body>
