3221bb374f0fc7423f5b28197be8ca33eabb0e1c
Jhonathan Moreira <jhongmoreira@gmail.com>
2020-07-17 09:52:07 -0300

    Tela de baixa de atendimento concluida

--- paginas/baixa_mv_aluno.php
+++ paginas/baixa_mv_aluno.php
@@ -1,15 +1,15 @@
 <?php
   include_once("includes/funcoes.php");
   $banco = new BancoDeDados;
-  $alunoId = $_GET['aluno'];
-  $banco->query("SELECT * FROM mv_aluno, alunos WHERE mv_aluno.matricula = alunos.id AND alunos.id = ".$alunoId);
+  $atendimentoID = $_GET['atendimento'];
+  $banco->query("SELECT mv_aluno.id as idMv, mv_aluno.matricula as mvMatricula, alunos.id as idAluno, alunos.nome as NomeAluno FROM mv_aluno, alunos WHERE mv_aluno.matricula = alunos.id AND mv_aluno.id = ".$atendimentoID);
   foreach ($banco->result() as $dados) {}
 ?>
 
 <!-- Título -->
 <div class="row p-2">
   <div class="col-md-12 cor-txt-padrao">
-    <h5><li class="fa fa-user-minus"></li> Dar baixa no atendimento n° <?php echo $dados['id']; ?> do aluno <?php echo $dados['nome']; ?> ?</h5>
+    <h5><li class="fa fa-user-minus"></li> Dar baixa no atendimento n° <?php echo $dados['idMv']; ?> do aluno <?php echo $dados['NomeAluno']; ?> ?</h5>
     <hr/>
   </div>
 </div>
@@ -32,14 +32,14 @@
 <?php
   if ($_SERVER["REQUEST_METHOD"] == 'POST')
   {
-    $banco->query("UPDATE mv_alunos SET estado = 1 WHERE id = ".$dados['id']);
+    $banco->query("UPDATE mv_aluno SET estado = 1 WHERE id = ".$dados['idMv']);
 
     $total = $banco->linhas();
 
     if ($total != 0)
     {
       echo "<div class='alert alert-info'>O atendimento teve sua baixa realizada</i></div>";
-      echo "<meta http-equiv='refresh' content='1;URL=index.php?pg=1'/>";
+      echo "<meta http-equiv='refresh' content='1;URL=index.php?pg=13'/>";
     }else
 
     {
